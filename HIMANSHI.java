90bb91b19df2845646f0e099fffdbf8e1521430c
Himanshii <himanshibhandarii01@gmail.com>
2025-11-21 09:24:22 +0545

    Initial sharing of project

---
+++ HIMANSHI.java
@@ -0,0 +1,67 @@
+
+/**
+ * Write a description of class HIMANSHI here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class HIMANSHI
+{
+    int qty;
+    static int rrt;
+    public static void main (String[] args) {
+        int age=18;
+        System.out.println(age);
+        System.out.println(HIMANSHI.rrt);
+        
+        //implict typecasting
+        
+        double dtr= age;
+        System.out.println(dtr);
+        
+        //explict typecasing
+        
+        double db= 10.09;
+        int itr= (int)db;
+        System.out.println(itr);
+        
+        //exceptions in arithmetic
+        
+        byte b1= 10;
+        byte b2= 11;
+        
+        byte sum= (byte) (b1+b2);
+        
+        //short, char
+        
+        
+        
+        //minimum, maximum, size, bytes
+        
+        System.out.println(Byte.MAX_VALUE);//MAXIMUM VALUE
+        System.out.println(Byte.MIN_VALUE);//MINIMUM VALUE
+        System.out.println(Byte.SIZE);//BITS
+        System.out.println(Byte.BYTES);//BYTES
+        
+        //escape sequence
+        
+        System.out.println("Hello\nWorld");
+        System.out.println("Hamro\nNepal");
+        System.out.println("She said\"hi\" ");
+        
+        
+        //Unicode Escape Sequence
+        
+        System.out.println("\u2764");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
